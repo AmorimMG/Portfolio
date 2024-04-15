c07448d263e892631453cc3ebbb8f0aa80d6f60c
Gabriel Amorim <gabriel._amorim@hotmail.com>
2024-04-15 17:00:07 -0300

    Passando as Requisicoes dos Endpoints para o NestJS, e adicionando background de Teste

--- src/service/endpoints.js
+++ src/service/endpoints.js
@@ -1,8 +1,10 @@
 const Endpoints = (instance) => ({
     ObterSteam: () => instance.get('steam'),
     ObterDiscord: () => instance.get('discord'),
-    ObterLastFM: (lastFMUsername) => instance.get(`lastfm?username=${lastFMUsername}`),
+    ObterLastFM: (lastFMUsername) => instance.get('lastfm?', { params: { username: lastFMUsername } }),
     EnviarEmail: (body) => instance.post('mail/send-email', body),
+    ObterSpotify: () => instance.get('spotify'),
+    ObterAnilist: (AnilistUsername) => instance.get(`anilist/${AnilistUsername}`),
 });
 
 export default Endpoints;
