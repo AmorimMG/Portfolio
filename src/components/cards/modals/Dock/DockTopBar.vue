e3d1c7bad329c3e4b61045affc2b8ad127152880
Gabriel Amorim <gabriel._amorim@hotmail.com>
2024-09-03 23:49:40 -0300

    alterando padrão para inglês

--- src/components/cards/modals/Dock/DockTopBar.vue
+++ src/components/cards/modals/Dock/DockTopBar.vue
@@ -1,189 +1,210 @@
 <script setup>
-import { ref, watchEffect, watch, onMounted, defineEmits, onUnmounted } from 'vue';
+import {
+	defineEmits,
+	onMounted,
+	onUnmounted,
+	ref,
+	watch,
+	watchEffect,
+} from "vue";
 
-import AppConfig from '../../../../layout/AppConfig.vue';
-import { useI18n } from 'vue-i18n';
-import { setLanguageCookie, getLanguageCookie } from '../../../../service/session';
+import { useI18n } from "vue-i18n";
+import AppConfig from "../../../../layout/AppConfig.vue";
+import {
+	getLanguageCookie,
+	setLanguageCookie,
+} from "../../../../service/session";
 
 const { locale } = useI18n();
 const dropdownValues = ref([
-    { name: 'Português', code: 'BR', value: 'pt' },
-    { name: 'Español', code: 'ES', value: 'es' },
-    { name: 'English', code: 'UK', value: 'en' }
+	{ name: "Português", code: "BR", value: "pt" },
+	{ name: "Español", code: "ES", value: "es" },
+	{ name: "English", code: "UK", value: "en" },
 ]);
 const dropdownValue = ref(null);
 
-const emit = defineEmits(['hide']);
+const emit = defineEmits(["hide"]);
 
 const onClose = () => {
-    emit('hide');
+	emit("hide");
 };
 
-const currentTime = ref('');
+const currentTime = ref("");
 const timer = ref(null);
 
 const updateTime = () => {
-    const now = new Date();
-    const options = { weekday: 'short', hour: '2-digit', minute: '2-digit' };
-    currentTime.value = now.toLocaleTimeString(locale.value, options);
+	const now = new Date();
+	const options = { weekday: "short", hour: "2-digit", minute: "2-digit" };
+	currentTime.value = now.toLocaleTimeString(locale.value, options);
 };
 
 onMounted(() => {
-    if (getLanguageCookie()) {
-        dropdownValue.value = dropdownValues.value.find((option) => option.value === getLanguageCookie());
-        locale.value = getLanguageCookie();
-    } else {
-        dropdownValue.value = dropdownValues.value.find((option) => option.value === 'pt');
-        locale.value = dropdownValue.value;
-    }
-    updateTime();
-    timer.value = setInterval(updateTime, 1000);
+	if (getLanguageCookie()) {
+		dropdownValue.value = dropdownValues.value.find(
+			(option) => option.value === getLanguageCookie(),
+		);
+		locale.value = getLanguageCookie();
+	} else {
+		dropdownValue.value = dropdownValues.value.find(
+			(option) => option.value === "en",
+		);
+		locale.value = dropdownValue.value;
+	}
+	updateTime();
+	timer.value = setInterval(updateTime, 1000);
 });
 
 onUnmounted(() => {
-    clearInterval(timer.value);
+	clearInterval(timer.value);
 });
 
 watchEffect(() => {
-    const cookieValue = getLanguageCookie();
-    if (cookieValue && dropdownValues.value.some((option) => option.value === cookieValue)) {
-        dropdownValue.value = dropdownValues.value.find((option) => option.value === cookieValue);
-    } else {
-        dropdownValue.value = dropdownValues.value.find((option) => option.value === 'pt');
-        locale.value = 'pt';
-    }
+	const cookieValue = getLanguageCookie();
+	if (
+		cookieValue &&
+		dropdownValues.value.some((option) => option.value === cookieValue)
+	) {
+		dropdownValue.value = dropdownValues.value.find(
+			(option) => option.value === cookieValue,
+		);
+	} else {
+		dropdownValue.value = dropdownValues.value.find(
+			(option) => option.value === "en",
+		);
+		locale.value = "en";
+	}
 });
 
 watch(dropdownValue, (newValue, oldValue) => {
-    if (newValue !== oldValue) {
-        setLanguageCookie(newValue.value);
-        locale.value = newValue.value;
-    }
+	if (newValue !== oldValue) {
+		setLanguageCookie(newValue.value);
+		locale.value = newValue.value;
+	}
 });
 
 const appConfigRef = ref(null);
 
 const menubarItems = ref([
-    {
-        label: 'Finder',
-        class: 'menubar-root'
-    },
-    {
-        label: 'File',
-        items: [
-            {
-                label: 'New',
-                icon: 'pi pi-fw pi-plus',
-                items: [
-                    {
-                        label: 'Bookmark',
-                        icon: 'pi pi-fw pi-bookmark'
-                    },
-                    {
-                        label: 'Video',
-                        icon: 'pi pi-fw pi-video'
-                    }
-                ]
-            },
-            {
-                label: 'Delete',
-                icon: 'pi pi-fw pi-trash'
-            },
-            {
-                separator: true
-            },
-            {
-                label: 'Export',
-                icon: 'pi pi-fw pi-external-link'
-            }
-        ]
-    },
-    {
-        label: 'Edit',
-        items: [
-            {
-                label: 'Left',
-                icon: 'pi pi-fw pi-align-left'
-            },
-            {
-                label: 'Right',
-                icon: 'pi pi-fw pi-align-right'
-            },
-            {
-                label: 'Center',
-                icon: 'pi pi-fw pi-align-center'
-            },
-            {
-                label: 'Justify',
-                icon: 'pi pi-fw pi-align-justify'
-            }
-        ]
-    },
-    {
-        label: 'Users',
-        items: [
-            {
-                label: 'New',
-                icon: 'pi pi-fw pi-user-plus'
-            },
-            {
-                label: 'Delete',
-                icon: 'pi pi-fw pi-user-minus'
-            },
-            {
-                label: 'Search',
-                icon: 'pi pi-fw pi-users',
-                items: [
-                    {
-                        label: 'Filter',
-                        icon: 'pi pi-fw pi-filter',
-                        items: [
-                            {
-                                label: 'Print',
-                                icon: 'pi pi-fw pi-print'
-                            }
-                        ]
-                    },
-                    {
-                        icon: 'pi pi-fw pi-bars',
-                        label: 'List'
-                    }
-                ]
-            }
-        ]
-    },
-    {
-        label: 'Events',
-        items: [
-            {
-                label: 'Edit',
-                icon: 'pi pi-fw pi-pencil',
-                items: [
-                    {
-                        label: 'Save',
-                        icon: 'pi pi-fw pi-calendar-plus'
-                    },
-                    {
-                        label: 'Delete',
-                        icon: 'pi pi-fw pi-calendar-minus'
-                    }
-                ]
-            },
-            {
-                label: 'Archive',
-                icon: 'pi pi-fw pi-calendar-times',
-                items: [
-                    {
-                        label: 'Remove',
-                        icon: 'pi pi-fw pi-calendar-minus'
-                    }
-                ]
-            }
-        ]
-    },
-    {
-        label: 'Quit'
-    }
+	{
+		label: "Finder",
+		class: "menubar-root",
+	},
+	{
+		label: "File",
+		items: [
+			{
+				label: "New",
+				icon: "pi pi-fw pi-plus",
+				items: [
+					{
+						label: "Bookmark",
+						icon: "pi pi-fw pi-bookmark",
+					},
+					{
+						label: "Video",
+						icon: "pi pi-fw pi-video",
+					},
+				],
+			},
+			{
+				label: "Delete",
+				icon: "pi pi-fw pi-trash",
+			},
+			{
+				separator: true,
+			},
+			{
+				label: "Export",
+				icon: "pi pi-fw pi-external-link",
+			},
+		],
+	},
+	{
+		label: "Edit",
+		items: [
+			{
+				label: "Left",
+				icon: "pi pi-fw pi-align-left",
+			},
+			{
+				label: "Right",
+				icon: "pi pi-fw pi-align-right",
+			},
+			{
+				label: "Center",
+				icon: "pi pi-fw pi-align-center",
+			},
+			{
+				label: "Justify",
+				icon: "pi pi-fw pi-align-justify",
+			},
+		],
+	},
+	{
+		label: "Users",
+		items: [
+			{
+				label: "New",
+				icon: "pi pi-fw pi-user-plus",
+			},
+			{
+				label: "Delete",
+				icon: "pi pi-fw pi-user-minus",
+			},
+			{
+				label: "Search",
+				icon: "pi pi-fw pi-users",
+				items: [
+					{
+						label: "Filter",
+						icon: "pi pi-fw pi-filter",
+						items: [
+							{
+								label: "Print",
+								icon: "pi pi-fw pi-print",
+							},
+						],
+					},
+					{
+						icon: "pi pi-fw pi-bars",
+						label: "List",
+					},
+				],
+			},
+		],
+	},
+	{
+		label: "Events",
+		items: [
+			{
+				label: "Edit",
+				icon: "pi pi-fw pi-pencil",
+				items: [
+					{
+						label: "Save",
+						icon: "pi pi-fw pi-calendar-plus",
+					},
+					{
+						label: "Delete",
+						icon: "pi pi-fw pi-calendar-minus",
+					},
+				],
+			},
+			{
+				label: "Archive",
+				icon: "pi pi-fw pi-calendar-times",
+				items: [
+					{
+						label: "Remove",
+						icon: "pi pi-fw pi-calendar-minus",
+					},
+				],
+			},
+		],
+	},
+	{
+		label: "Quit",
+	},
 ]);
 </script>
 
--- src/utils/exportCsv.js
+++ src/utils/exportCsv.js
@@ -1,22 +1,24 @@
-export function exportCSV(data, fileName = 'export.csv') {
-    let csvContent = 'data:text/csv;charset=utf-8,';
+export function exportCSV(data, fileName = "export.csv") {
+	let csvContent = "data:text/csv;charset=utf-8,";
 
-    if (data.length > 0) {
-        const headers = Object.keys(data[0]);
-        csvContent += headers.join(',') + '\r\n';
-    }
+	if (data.length > 0) {
+		const headers = Object.keys(data[0]);
+		csvContent += `${headers.join(",")}\r\n`;
+	}
 
-    data.forEach((row) => {
-        const values = Object.values(row).map((value) => String(value).replace(/,/g, ''));
-        csvContent += values.join(',') + '\r\n';
-    });
+	for (const row of data) {
+		const values = Object.values(row).map((value) =>
+			String(value).replace(/,/g, ""),
+		);
+		csvContent += `${values.join(",")}\r\n`;
+	}
 
-    const encodedUri = encodeURI(csvContent);
-    const link = document.createElement('a');
-    link.setAttribute('href', encodedUri);
-    link.setAttribute('download', fileName);
-    document.body.appendChild(link);
+	const encodedUri = encodeURI(csvContent);
+	const link = document.createElement("a");
+	link.setAttribute("href", encodedUri);
+	link.setAttribute("download", fileName);
+	document.body.appendChild(link);
 
-    link.click();
-    document.body.removeChild(link);
+	link.click();
+	document.body.removeChild(link);
 }
