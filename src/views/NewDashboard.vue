d04bed2f583da71c2de91f4b239665bdf639bf45
Gabriel Amorim <gabriel._amorim@hotmail.com>
2025-03-26 16:20:46 -0300

    :sparkles: feat: removendo v-else para melhora de performance

--- src/views/NewDashboard.vue
+++ src/views/NewDashboard.vue
@@ -13,23 +13,10 @@ onMounted(() => {
 </script>
 
 <template>
-<!-- <div v-if="!isStarted" class="login-screen center">
-    <div style="position: relative;">
-        <div style="position: absolute; top: -5px; left: -5px; width: 110px; height: 110px; border: 5px solid black; border-radius: 25%;"></div>
-        <Image :src="AmorimLogo" alt="Image" width="100" style="top: -1px;" />
-    </div>
-    <h5 class="mt-3">Amorim Portfolio</h5>
-    <div class="flex gap-2">
-        <Button @click="isStarted = true" icon="pi pi-arrow-left" outlined class="p-button-rounded" />
-        <Password placeholder="*****" :feedback="false" />
-        <Button icon="pi pi-question" outlined class="p-button-rounded"/>
-    </div>
-    <img width="100%" height="100%" class="background-container" :src="MacOSBackground" />
-</div> -->
 <div v-if="isStarted" class="center">
     <BootupScreen/>
 </div>
-<Dock :class="{ 'hidden': isStarted, 'fade-in': !isStarted }" v-else />
+<Dock :class="{ 'hidden': isStarted, 'fade-in': !isStarted }" />
 </template>
 
 <style scoped>
